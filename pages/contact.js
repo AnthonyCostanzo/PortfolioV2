fe29fb2aba5df9c2fdd9460f0ceaf479176800ba
Anthony Costanzo <anthonycostanzo23@gmail.com>
2022-07-28 16:56:45 -0400

    a

--- pages/contact.js
+++ pages/contact.js
@@ -2,87 +2,133 @@ import Layout from "../components/Layout";
 import { BsFillTelephoneFill, BsGithub, BsLinkedin } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
 import { ImLocation } from "react-icons/im";
+import { useRef, useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+import emailjs from "emailjs-com";
 
-const Contact = () => (
-  <Layout>
-    <form className=" font-['Mono'] pb-20">
-      <h1 className="m-auto relative left-4 md:right-4 px-3 text-3xl mt-10 font-semibold border-black border-r-2 border-l-2 w-max">
-        Contact Me
-      </h1>
-      <div className="flex ">
-        <div className="grid grid-cols-1 md:grid-cols-2 mt-10  lg:mt-20 lg:w-8/12 m-auto">
-          <div className="grid gap-3 ml-10 gap-x-5 ">
-            <input
-              className=" w-72 lg:w-[40vw] p-2 h-12 rounded-sm border-black border-[1px]"
-              type="text"
-              placeholder=" name"
-              name="name"
-            />
-            <input
-              className="w-72 lg:w-[40vw] p-2 h-12 rounded-sm border-black border-[1px]"
-              type="email"
-              placeholder=" email"
-              name="email"
-            />
-            <input
-              className="w-72 lg:w-[40vw] p-2 h-12 rounded-sm border-black border-[1px]"
-              type="email"
-              placeholder=" subject"
-              name="email"
-            />
-            <textarea
-              className="w-72 lg:w-[40vw] p-2 h-32 rounded-sm border-black border-[1px]"
-              placeholder="Enter your message"
-            ></textarea>
-            <button className="w-24 rounded-sm hover:bg-black hover:text-white border-[1.4px] border-black h-10 text-[17px]">
-              Send
-            </button>
-          </div>
-          <div className=" ml-10 mt-10 md:ml-14 md:mt-2 lg:ml-44 space-y-5">
-            <div className="flex">
-              <BsFillTelephoneFill
-                className="hover:scale-110 cursor-pointer"
-                size={17}
+const Contact = ({ email_id, client_id, template_id }) => {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const messageRef = useRef();
+  const [isLoading, setIsLoading] = useState(false);
+  const onFormSubmit = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const message = messageRef.current.value;
+
+    try {
+      await emailjs.send(
+        email_id,
+        template_id,
+        {
+          name,
+          email,
+          phone: "not provided",
+          message,
+        },
+        client_id
+      );
+      alert("Message sent succesfully!");
+    } catch (err) {
+      console.log(err);
+    }
+    setIsLoading(false);
+  };
+  return (
+    <Layout>
+      {isLoading && (
+        <div className="text-center">
+          <ClipLoader size={50} color="black" />
+        </div>
+      )}
+      <form onSubmit={onFormSubmit} className=" font-['Mono'] pb-20">
+        <h1 className="m-auto relative left-4 md:right-4 px-3 text-3xl mt-10 font-semibold border-black border-r-2 border-l-2 w-max">
+          Contact Me
+        </h1>
+        <div className="flex ">
+          <div className="grid grid-cols-1 md:grid-cols-2 mt-10  lg:mt-20 lg:w-8/12 m-auto">
+            <div className="grid gap-3 ml-10 gap-x-5 ">
+              <input
+                ref={nameRef}
+                className=" w-72 lg:w-[40vw] p-2 h-12 rounded-sm border-black border-[1px]"
+                type="text"
+                placeholder=" name"
+                name="name"
               />
-              <span className="ml-3 hover:font-bold hover:scale-105 cursor-pointer">
-                617-953-6678
-              </span>
-            </div>
-            <div className="flex">
-              <MdEmail className="hover:scale-110 cursor-pointer" size={17} />
-              <span className="ml-3 hover:font-bold cursor-pointer">
-                anthonycostanzo23@gmail.com
-              </span>
-            </div>
-            <div className="flex">
-              <ImLocation
-                className="hover:scale-110 cursor-pointer"
-                size={17}
+              <input
+                ref={emailRef}
+                className="w-72 lg:w-[40vw] p-2 h-12 rounded-sm border-black border-[1px]"
+                type="email"
+                placeholder=" email"
+                name="email"
               />
-              <span className="ml-3 hover:font-bold cursor-pointer">
-                Staten Island NY
-              </span>
+
+              <textarea
+                ref={messageRef}
+                className="w-72 lg:w-[40vw] p-2 h-32 rounded-sm border-black border-[1px]"
+                placeholder="Enter your message"
+              ></textarea>
+              <button className="w-24 rounded-sm hover:bg-black hover:text-white border-[1.4px] border-black h-10 text-[17px]">
+                Send
+              </button>
             </div>
-            <div className="">
-              <h3 className="text-xl font-bold">
-                Follow me on Github & Linkedin
-              </h3>
-              <div className="flex mt-4 w-24 space-x-5">
-                <BsGithub
+            <div className=" ml-10 mt-10 md:ml-14 md:mt-2 lg:ml-44 space-y-5">
+              <div className="flex">
+                <BsFillTelephoneFill
                   className="hover:scale-110 cursor-pointer"
                   size={17}
                 />
-                <BsLinkedin
+                <span className="ml-3 hover:font-bold hover:scale-105 cursor-pointer">
+                  617-953-6678
+                </span>
+              </div>
+              <div className="flex">
+                <MdEmail className="hover:scale-110 cursor-pointer" size={17} />
+                <span className="ml-3 hover:font-bold cursor-pointer">
+                  anthonycostanzo23@gmail.com
+                </span>
+              </div>
+              <div className="flex">
+                <ImLocation
                   className="hover:scale-110 cursor-pointer"
                   size={17}
                 />
+                <span className="ml-3 hover:font-bold cursor-pointer">
+                  Staten Island NY
+                </span>
+              </div>
+              <div className="">
+                <h3 className="text-xl font-bold">
+                  Follow me on Github & Linkedin
+                </h3>
+                <div className="flex mt-4 w-24 space-x-5">
+                  <BsGithub
+                    className="hover:scale-110 cursor-pointer"
+                    size={17}
+                  />
+                  <BsLinkedin
+                    className="hover:scale-110 cursor-pointer"
+                    size={17}
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </form>
-  </Layout>
-);
-
+      </form>
+    </Layout>
+  );
+};
 export default Contact;
+
+export const getStaticProps = () => {
+  return {
+    props: {
+      email_id: process.env.EMAIL_ID,
+      client_id: process.env.CLIENT_ID,
+      template_id: process.env.TEMPLATE_ID,
+    },
+  };
+};
